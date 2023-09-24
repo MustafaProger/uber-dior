97f90bd885fd0682118a04c7dad03def8fc6ac1a
DIOR <diertojiev@yandex.ru>
2023-09-24 21:04:26 +0300

    Сборка частей footer и navbar
    Я создал js файлы, в которые местил footer и navbar, дабы не прописывать его несколько раз везде

---
+++ src/js/footer.js
@@ -0,0 +1,56 @@
+document.getElementById('footer').innerHTML = '\
+<section class="footer">\
+    <div class="line"></div>\
+    <div class="content row">\
+        <div class="col-md-3 order_media">\
+            <div class="media">\
+                <a href="https://www.facebook.com/uber?uclick_id=06382d58-faf9-439f-a579-c3987300798e"\
+                    target="_blank" class="media__item facebook" style=""><img src="../src/icons/footer/media/facebook.svg"\
+                        alt="">\</a>\
+                <a href="https://twitter.com/uber?uclick_id=06382d58-faf9-439f-a579-c3987300798e"\
+                    target="_blank" class="media__item twitter"><img src="../src/icons/footer/media/twitter.svg" alt=""></a>\
+                <a href="https://www.linkedin.com/uas/login?session_redirect=%2Fcompany%2F1815218%3Fuclick_id%3D06382d58-faf9-439f-a579-c3987300798e"\
+                    target="_blank" class="media__item linkdedin"><img src="../src/icons/footer/media/linkedin.svg"\
+                        alt=""></a>\
+                <a href="https://www.instagram.com/uber/?uclick_id=06382d58-faf9-439f-a579-c3987300798e"\
+                    target="_blank" class="media__item instagram"><img src="../src/icons/footer/media/instagram.svg"\
+                        alt=""></a>\
+            </div>\
+        </div>\
+        <div class="col-md-6 order_link">\
+            <div class="link">\
+                <div class="toplink">\
+                    <a href="index.html#promo" class="toplink__item">На главную</a>\
+                    <a href="cities.html" class="toplink__item" style="padding: 0 10px">Города</a>\
+                    <a href="#" class="toplink__item">Для водителей</a>\
+                </div>\
+                <div class="sublink">\
+                    <a href="#" class="sublink__item">Центр поддержки</a>\
+                    <a href="#" class="sublink__item">Вакансии</a>\
+                    <a href="#" class="sublink__item">Разработчики</a>\
+                    <a href="#" class="sublink__item">Блог</a>\
+                    <a href="#" class="sublink__item">О нас</a>\
+                </div>\
+                <div class="language">\
+                    <a href="#" class="">Русский</a>\
+                </div>\
+            </div>\
+        </div>\
+        <div class="col-md-3 order_download">\
+            <div class="download">\
+                <a href="https://apps.apple.com/ru/app/uber-%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7-%D0%BF%D0%BE%D0%B5%D0%B7%D0%B4%D0%BE%D0%BA/id368677368"\
+                    target="_blank" class="download__item"><img src="../src/icons/footer/download/apple-btn.svg"\
+                        alt="" style="width: 80px; height: 80px;"></a>\
+                <a href="https://play.google.com/store/apps/details?id=com.ubercab&hl=ru&gl=US&pli=1"\
+                    target="_blank" class="download__item" id="download__item_2"><img\
+                        src="../src/icons/footer/download/google-btn.svg" alt=""\
+                        style="width: 80px; height: 80px;"></a>\
+                <a href="https://apps.microsoft.com/store/detail/uber/9WZDNCRFHXRD?hl=ru-ru&gl=ru" target="_blank"\
+                    class="download__item" id="download__item_3"><img\
+                        src="../src/icons/footer/download/window-btn.svg" alt=""\
+                        style="width: 80px; height: 80px;"></a>\
+            </div>\
+        </div>\
+    </div>\
+</section>\
+'
---
+++ src/js/navbar.js
@@ -0,0 +1,23 @@
+document.getElementById("navbar").innerHTML = '\
+<main class="main__page">\
+    <header class="header">\
+        <div class="top__header">\
+            <div class="container">\
+                <div class="logo_without_text"></div>\
+                <div class="header__burger">\
+                </div>\
+                <nav class="header__menu">\
+                    <ul class="header__list">\
+                        <li><a href="index.html#promo" class="header__link">Главная</a></li>\
+                        <li><a href="yourcar.html" class="header__link">Со своим автомобилем</a></li>\
+                        <li><a href="" class="header__link">На автомобили компании</a></li>\
+                        <li><a href="index.html#condition" class="header__link">Требования</a></li>\
+                        <li><a href="" class="header__link">Контакты</a></li>\
+                        <li><a href="" class="header__link">Оставить заявку</a></li>\
+                    </ul>\
+                </nav>\
+            </div>\
+        </div>\
+    </header>\
+</main>\
+'
