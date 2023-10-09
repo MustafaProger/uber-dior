8528e92d2f9ed24a2f4ce49224d355743277a3da
DIOR <diertojiev@yandex.ru>
2023-10-09 16:52:01 +0300

    Недоделанная отправка на почту и небольшие корректировки

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+"use strict"
+
+// затемнение и фиксирование заднего фона при выхода меню бургер
 $(document).ready(function () {
     $('.header__burger').click(function (event) {
         $(".header__burger").toggleClass('active');
@@ -18,21 +21,54 @@ $(document).ready(function () {
     });
 });
 
-function scrollTo(element) {
-    window.scroll({
-        left: 0,
-        top: element.offsetTop,
-        behavior: "smooth",
-    })
-}
 
-window.onload=function(){
 
-    let IDname = document.querySelector("#id");
-    let scrollIDname = document.querySelector("#id");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
 
-    IDname.addEventListener('click', () => {
-        scrollTo(scrollIDname);
-        console.log('Все работает');
-    }); 
-}
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll("._req");
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input)
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === "") {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+    }
+
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+    function emailTest(input) {
+       return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+});
